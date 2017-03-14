2a096276f7c142169076cb7e4b0f30cc8e1abdd0
konglingkai <konglingkai@ajmide.com>
2017-03-14 10:31:42 +0800

    添加pm2配置。

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : "Oracle",
+      script    : "index.js",
+      env: {
+        COMMON_VARIABLE: "true"
+      },
+      env_production : {
+        NODE_ENV: "production"
+      }
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : "ash",
+      host : "121.40.197.93",
+      ref  : "origin/master",
+      repo : "git@github.com:ashnorseman/oracle-bone.git",
+      path : "/home/ash/oracle",
+      "post-deploy" : "npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production"
+    }
+  }
+}
